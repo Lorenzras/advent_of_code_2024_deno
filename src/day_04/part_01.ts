559422865b1edb1c739b1ccd7f69d2826d3df7a9
Lorenz Ras <lenzras@gmail.com>
2024-12-07 18:13:42 +0900

    add solution for the sample input of day 4 part 1

--- src/day_04/part_01.ts
+++ src/day_04/part_01.ts
@@ -1,5 +1,132 @@
-export default function (input: string): number {
-  console.log(input);
+const convertToMatrix = (input: string): string[][] => {
+  return input.split("\n").map((x) => x.split(""));
+};
 
-  return 0;
+const countWord = (matrix: string[][], word: string): number => {
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const wordLength = word.length;
+
+  let count = 0;
+
+  for (let row = 0; row < rows; row++) {
+    for (let col = 0; col < cols; col++) {
+      // checkForward
+      const maxForwardIndex = col + wordLength;
+      if (maxForwardIndex <= rows) {
+        const wordToCheck = matrix[row].slice(col, maxForwardIndex).join("");
+        if (wordToCheck === word) {
+          count++;
+        }
+      }
+
+      // checkReverse
+      const maxReverseIndex = col - wordLength + 1;
+      if (maxReverseIndex >= 0) {
+        const wordToCheckArr = [];
+
+        for (let i = col; i >= maxReverseIndex; i--) {
+          wordToCheckArr.push(matrix[row][i]);
+        }
+
+        const wordToCheck = wordToCheckArr.join("");
+
+        if (wordToCheck === word) {
+          count++;
+        }
+      }
+
+      // checkDown
+      const maxDownIndex = row + wordLength;
+      if (maxDownIndex <= cols) {
+        const wordToCheckArr = [];
+
+        for (let i = row; i < maxDownIndex; i++) {
+          wordToCheckArr.push(matrix[i][col]);
+        }
+
+        const wordToCheck = wordToCheckArr.join("");
+        if (wordToCheck === word) {
+          count++;
+        }
+      }
+
+      // checkReverseUp
+      const maxReverseUpIndex = row - wordLength + 1;
+      if (maxReverseUpIndex >= 0) {
+        const wordToCheckArr = [];
+
+        for (let i = row; i >= maxReverseUpIndex; i--) {
+          wordToCheckArr.push(matrix[i][col]);
+        }
+
+        const wordToCheck = wordToCheckArr.join("");
+        if (wordToCheck === word) {
+          count++;
+        }
+      }
+
+      // checkDiagonalDownRight
+      if (maxForwardIndex <= rows && maxDownIndex <= cols) {
+        const wordToCheckArr = [];
+        for (let i = 0; i < wordLength; i++) {
+          wordToCheckArr.push(matrix[row + i][col + i]);
+        }
+
+        const wordToCheck = wordToCheckArr.join("");
+        if (wordToCheck === word) {
+          count++;
+        }
+      }
+
+      // checkDiagonalDownLeft
+      if (maxReverseIndex >= 0 && maxDownIndex <= cols) {
+        const wordToCheckArr = [];
+        for (let i = 0; i < wordLength; i++) {
+          wordToCheckArr.push(matrix[row + i][col - i]);
+        }
+
+        const wordToCheck = wordToCheckArr.join("");
+        if (wordToCheck === word) {
+          count++;
+        }
+      }
+
+      // checkDiagonalUpRight
+      if (maxForwardIndex <= rows && maxReverseUpIndex >= 0) {
+        const wordToCheckArr = [];
+        for (let i = 0; i < wordLength; i++) {
+          wordToCheckArr.push(matrix[row - i][col + i]);
+        }
+
+        const wordToCheck = wordToCheckArr.join("");
+        if (wordToCheck === word) {
+          count++;
+        }
+      }
+
+      // checkDiagonalUpLeft
+      if (maxReverseIndex >= 0 && maxReverseUpIndex >= 0) {
+        const wordToCheckArr = [];
+        for (let i = 0; i < wordLength; i++) {
+          wordToCheckArr.push(matrix[row - i][col - i]);
+        }
+
+        const wordToCheck = wordToCheckArr.join("");
+        if (wordToCheck === word) {
+          console.log(wordToCheck, row, col);
+          count++;
+        }
+      }
+    }
+  }
+
+  return count;
+};
+
+export default function (input: string): number {
+  const inputMatrix = convertToMatrix(input);
+  const xmasCount = countWord(inputMatrix, "XMAS");
+
+  return xmasCount;
 }
