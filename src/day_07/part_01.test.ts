2a79a94e4764b751c63d6fbd132c03f4d0816d70
Lorenz Ras <lenzras@gmail.com>
2024-12-08 21:22:40 +0900

    add solution to day 7 part 1

--- src/day_07/part_01.test.ts
+++ src/day_07/part_01.test.ts
@@ -4,15 +4,22 @@ import f from "./part_01.ts";
 const testCases = [
   {
     filename: "input_sample.txt",
-    expected: 0,
+    expected: 3749,
+  },
+  {
+    filename: "input.txt",
+    expected: 5540634308362,
   },
 ];
 
 for (const { filename, expected } of testCases) {
   Deno.test("should return the correct answer", () => {
     const input = Deno.readTextFileSync(`${import.meta.dirname}/${filename}`);
+    const start = performance.now();
     const result = f(input);
+    const end = performance.now();
     console.log(`Result: ${result} (${filename})`);
+    console.log(`Runtime: ${end - start}ms (${filename})`);
     assertEquals(result, expected);
   });
 }
--- src/day_07/part_01.ts
+++ src/day_07/part_01.ts
@@ -1,3 +1,72 @@
+enum Opt {
+  ADD,
+  MUL,
+}
+
+const operatorMapByLength: Map<number, Opt[][]> = new Map();
+
+const generateCombinations = (length: number): Opt[][] => {
+  const combinations: Opt[][] = [];
+  const totalCombinations = 2 ** length;
+
+  for (let i = 0; i < totalCombinations; i++) {
+    const combination: Opt[] = [];
+    for (let j = 0; j < length; j++) {
+      combination.push(i & (1 << j) ? Opt.MUL : Opt.ADD);
+    }
+    combinations.push(combination);
+  }
+
+  return combinations;
+};
+
+const applyOperators = (operators: Opt[], values: number[]): number => {
+  let result = values[0];
+
+  for (let i = 1; i < values.length; i++) {
+    switch (operators[i - 1]) {
+      case Opt.ADD:
+        result += values[i];
+        break;
+      case Opt.MUL:
+        result *= values[i];
+        break;
+    }
+  }
+
+  return result;
+};
+
+const parseInput = (input: string) => {
+  const parsedInput: [number, number[]][] = input.split("\n").map((row) => {
+    const [result, values] = row.split(": ");
+    return [Number(result), values.split(" ").map(Number)];
+  });
+
+  return parsedInput;
+};
+
 export default function (input: string): number {
-  return 0;
+  const parsedInput = parseInput(input);
+  let total = 0;
+
+  for (const [result, values] of parsedInput) {
+    const length = values.length - 1;
+
+    if (!operatorMapByLength.has(length)) {
+      operatorMapByLength.set(length, generateCombinations(length));
+    }
+
+    const optCombinations = operatorMapByLength.get(length)!;
+
+    for (const optCombination of optCombinations) {
+      const resultFromCombination = applyOperators(optCombination, values);
+      if (resultFromCombination === result) {
+        total += resultFromCombination;
+        break;
+      }
+    }
+  }
+
+  return total;
 }
